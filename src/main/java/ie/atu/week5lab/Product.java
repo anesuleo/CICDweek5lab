175b4b8023924e90336bc5547670c6ec03924c0e
G00425238@atu.ie <g00425238@atu,ie>
2024-10-22 11:12:21 +0100

    Created new project and created product service and controller

--- src/main/java/ie/atu/week5lab/Product.java
+++ src/main/java/ie/atu/week5lab/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week5lab;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @NotNull(message="This value can not be 0")
     private int id;
+    @NotBlank(message="This field can not be blank")
     private String name;
+    @Positive(message="Price has to be greater than 0")
     private double price;
 
 }
